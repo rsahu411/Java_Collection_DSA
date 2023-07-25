136f1d6eaac8349fef3ef5baef0d141f6d79ac69
rishabh <rishabsahu32@gmail.com>
2023-07-25 11:38:03 +0530

    Adding ArrayList

---
+++ src/List/ArrayListClass.java
@@ -0,0 +1,102 @@
+package List;
+
+import com.sun.source.tree.ArrayAccessTree;
+
+import java.util.*;
+
+public class ArrayListClass {
+
+    public void arrayList()
+    {
+//        ArrayList al = new ArrayList();
+
+        List<Object> al = new ArrayList<>();
+
+        //1. add()
+        al.add(20);
+        al.add(32.78);
+        al.add(45);
+        al.add('R');
+        al.add("Rishabh");
+        al.add(true);
+
+        //print()
+        System.out.println(al);
+
+        //2. size()
+        System.out.println("Size of ArrayList : "+al.size());
+
+        //3. remove()
+        al.remove(2);
+        System.out.println("ArrayList after removing a element: "+al);
+
+        //4. add(index,object)
+        al.add(3,"Java");
+        System.out.println("ArrayList after adding element at particular position:"+ al);
+
+        //5. retrieve a element
+        System.out.println("Get a element at particular index: "+al.get(3));
+
+        //6. change element
+        al.set(3,"SpringBoot");
+        System.out.println("Change existing value: "+al);
+
+        //7. search - contains()
+        System.out.println("Search element in list: "+al.contains('R'));
+        System.out.println("Search element in list: "+al.contains("Java"));
+
+        //8. isEmpty()
+        System.out.println("List is Empty: "+al.isEmpty());
+
+        //9. for()
+        System.out.print("Reading elements using for loop: ");
+        for(int i=0;i<al.size();i++)
+            System.out.print(al.get(i)+" ");
+        System.out.println();
+
+        //10. for..Each
+        System.out.print("Reading elements using for each loop: ");
+        for(Object list : al)
+            System.out.print(list+" ");
+        System.out.println();
+
+        // 11. Iterator
+        System.out.print("Reading elements using iterator: ");
+        Iterator<Object> iterator = al.iterator();
+        while(iterator.hasNext())
+        {
+            System.out.print(iterator.next()+" ");
+        }
+        System.out.println();
+
+
+        //12. addAll()
+        List al1 = new ArrayList<>();
+        al1.add("X");
+        al1.add("Y");
+        al1.add("Z");
+        al1.add("A");
+        al1.add("B");
+        al1.add("C");
+
+        ArrayList<Object> al1_copy = new ArrayList<>();
+        al1_copy.addAll(al1);
+        System.out.println("addAll Method: "+al1_copy);
+
+        //13. removeAll()
+        al1_copy.removeAll(al1);
+        System.out.println("After Removal: "+al1);
+
+        //14. Collections.sort()
+        Collections.sort(al1);
+        System.out.println("Sorted ArrayList: "+al1);
+
+        //15. Collections.sort - reverseOrder
+        Collections.sort(al1,Collections.reverseOrder());
+        System.out.println("Sorted arrayList in reverse order: "+al1);
+
+        //15. Collections.shuffle()
+        Collections.shuffle(al1);
+        System.out.println("Shuffle arrayList: "+al1);
+    }
+}
--- src/List/ListMain.java
+++ src/List/ListMain.java
@@ -2,5 +2,10 @@ package List;
 
 public class ListMain {
 
+    public static void main(String[] args) {
 
+        ArrayListClass object = new ArrayListClass();
+
+        object.arrayList();
+    }
 }
